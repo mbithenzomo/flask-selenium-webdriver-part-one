b5c85b5752795227f4e0e464265d4518eebb9d0f
Mbithe Nzomo <mbithe.nzomo@andela.com>
2017-01-05 12:04:24 +0300

    Add .gitignore file

--- instance/config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-SECRET_KEY = 'p9Bv<3Eid9%$i01'
-if os.getenv('FLASK_CONFIG') == "testing":
-    SQLALCHEMY_DATABASE_URI = 'mysql://dt_admin:dt2016@localhost/dreamteam_test'
-else:
-    SQLALCHEMY_DATABASE_URI = 'mysql://dt_admin:dt2016@localhost/dreamteam_db'
